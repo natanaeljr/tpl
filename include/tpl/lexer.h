dd5e9600c24885bfacd9b6834de9298e2362b5a6
Natanael Rabello <natanael.rabello@outlook.com>
2019-07-03 15:53:10 -0300

    Document Lexer private methods

--- include/tpl/lexer.h
+++ include/tpl/lexer.h
@@ -18,7 +18,6 @@ class Lexer {
    public:
     /**
      * \brief Scan a stream for lexemes and generate tokens.
-     * Convert the sequence of characters into a sequence of tokens.
      */
     static std::list<Token> Scan(std::istream& istream) noexcept(false)
     {
@@ -28,22 +27,23 @@ class Lexer {
     /***********************************************************************************/
    private:
     /**
-     * \brief
+     * \brief Convert the sequence of characters into a sequence of tokens.
      */
     std::list<Token> ScanTokens(std::istream& istream) noexcept(false);
 
     /**
-     * \brief
+     * \brief Scan a string lexeme until terminator.
+     * First character in the istream is expected to be the start of the lexeme.
      */
     std::string ScanString(std::istream& istream, const char terminator) noexcept(false);
 
     /**
-     * \brief
+     * \brief Scan a identifier lexeme.
      */
     std::string ScanIdentifier(std::istream& istream) noexcept(true);
 
     /**
-     * \brief
+     * \brief Scan a decimal lexeme.
      */
     std::string ScanDecimal(std::istream& istream) noexcept(true);
 
